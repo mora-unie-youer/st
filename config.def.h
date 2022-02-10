dbce71c6c541150d9d00242ab771f8b7a35fdbc2
Mora Unie Youer <mora_unie_youer@riseup.net>
2022-02-10 20:34:02 +0000

    Set FiraCode font

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@ char *externalpipe_sigusr1[] = {"/bin/sh", "-c", "externalpipe_buffer.sh st_stri
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font Mono:pixelsize=11:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
